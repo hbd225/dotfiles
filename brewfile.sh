cb26c8be2e3cb5c2d675c8b0f677a7b750de835b
Norimasa Ando <norimasa.ando@gmail.com>
2017-05-31 20:03:24 +0900

    update brewfile.sh

--- brewfile.sh
+++ brewfile.sh
@@ -13,7 +13,6 @@ brew install git
 brew install wget
 brew install coreutils
 brew install vim
-brew install subversion
 brew install rbenv
 brew install ruby-build
 brew install tmux
@@ -22,9 +21,13 @@ brew install ag
 brew install tig
 brew install postgres
 brew install mysql
+brew install mongodb
 brew install memcached
 brew install redis
 brew install nginx
+brew install peco
+brew install curl
+brew install flow
 
 brew cask install vagrant
 brew cask install virtualbox
